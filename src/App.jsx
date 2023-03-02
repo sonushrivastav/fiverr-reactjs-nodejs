4c0183c4be089897f2fd9d6f220b826aac4d3cee
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-02 21:12:34 +0530

    styling navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Navbar from './components/navbar/Navbar'
 function App() {
 
   return (
-    <div className="App">
+    < >
       <Navbar/>
-    </div>
+    </>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import "./navbar.css"
+import React from "react";
+import "./navbar.css";
 const Navbar = () => {
   return (
-    <div className='navbar'>
-          <div className='container'>
-              <div className='logo'>
-                  <span className='text'>fiverr</span>
-                  <span className='dots'>.</span>
-              </div>
-              <div className='links'>
-                  <span>fiverr Buisness</span>
-                  <span>Explore</span>
-                  <span>English</span>
-                  <span>Sign in</span>
-                  <span>Become a Seller</span>
-                  <button>Join</button>
-              </div>
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <span className="text">fiverr</span>
+          <span className="dots">.</span>
+        </div>
+        <div className="links">
+          <span>Fiverr Buisness</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          <span>Become a Seller</span>
+          <button>Join</button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
