ee9badb9af78d460910b1ba1bc6b2cfe6ff53fc4
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-02 21:47:25 +0530

    added animation on navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // import { Link } from "react-router-dom";
 import "./navbar.css";
 const Navbar = () => {
 
-  const[active,setActive]=useState(true)
+  const [active, setActive] = useState(false)
+  
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false)
+  }
 
+  useEffect(() => {
+    window.addEventListener("scroll", isActive)
+
+    return () => {
+      window.removeEventListener("scroll",isActive)
+    }
+  },[])
   return (
     <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
@@ -23,11 +34,18 @@ const Navbar = () => {
           <button>Join</button>
         </div>
       </div>
+      {
+        active && (
+          <>
+
       <hr />
       <div className="menu">
         <span>Test</span>
         <span>Test2</span>
       </div>
+          </>
+        )
+      }
     </div>
   );
 };
