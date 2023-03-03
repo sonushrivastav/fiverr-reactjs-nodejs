6590d916ea63955fd01b8cdf4142c909736c3e15
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-03 21:29:08 +0530

    added footer component and router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import Home from "./pages/home/Home"
 import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
+  const Layout = () => {
+    return (
+      <>
+        <div className='app'>
+         <Navbar/>
+         <Outlet/>
+         <Footer/>
+        </div>
+       </>
+     )
+   }
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+    },
+  ]);
   return (
-    < >
-      <Navbar />
+    <>
+      <RouterProvider router={ router} />
       
     </>
   )
