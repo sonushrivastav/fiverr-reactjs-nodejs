a632139b6fe5976931e2223cd0fa4f00cd0f30bd
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-04 15:17:26 +0530

    modifying css over the component in order to maintain aspect ration of website

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Featured from '../../components/featured/Featured'
+import TrustedBy from '../../components/trustedby/TrustedBy'
 import "./home.css"
 const Home = () => {
   return (
     <div>
-      <Featured/>
+      <Featured />
+      <TrustedBy/>
     </div>
   )
 }
