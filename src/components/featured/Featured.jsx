bd5aa1b8f04be36f816c6d3226f898641e7aff09
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-04 14:41:17 +0530

    added featured component

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./featured.css"
+const Featured = () => {
+  return (
+    <div className='featured'>
+          <div className='container'>
+              <div className='left'>
+                  <h1>Find the perfect freelance services for your buisness</h1>
+                  <div className='search'>
+                      <div className='searchInput'>
+                          <img src="https://raw.githubusercontent.com/safak/youtube23/fiverr-ui/public/img/search.png" alt="" />
+                          <input type="text" placeholder='Try building mobile app'/>
+                      </div>
+                      <button>Search</button>
+                  </div>
+                  <div className='popular'>
+                      <span>Popular:</span>
+                      <button>Web Design</button>
+                      <button>Logo Design</button>
+                      <button>AI Services</button>
+                      <button>Cloud Services</button>
+                  </div>
+              </div>
+              <div className='right'>
+                  <img src="" alt="" />
+              </div>
+      </div>
+    </div>
+  )
+}
+
+export default Featured
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Featured from '../../components/featured/Featured'
 import "./home.css"
 const Home = () => {
   return (
     <div>
-      Home
+      <Featured/>
     </div>
   )
 }
