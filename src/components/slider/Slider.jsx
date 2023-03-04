39461acd55a35ca5f9ffc9144ef570bd4c9f0725
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-04 15:30:49 +0530

    added slider component

---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./slider.css"
+const Slider = () => {
+  return (
+    <div className='slider'>
+          <div className='containerr'>
+              
+      </div>
+    </div>
+  )
+}
+
+export default Slider
