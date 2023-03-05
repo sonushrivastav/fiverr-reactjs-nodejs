4960dea884ff676d66b53b9630a98ea394686444
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-05 19:21:25 +0530

    adding video for feature

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,9 +6,10 @@ import "./home.css"
 import { cards } from '../../data'
 import CategoryCard from '../../components/categoryCard/CategoryCard'
 import check from "../../assets/check.png"
+import video from "../../assets/video.mp4"
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
       <Featured />
       <TrustedBy />
       <Slide slidesToShow={4} arrowsScroll={3}>
@@ -29,9 +30,40 @@ const Home = () => {
             <p>
               Find high-quality services at every price point. No hourly rates, just project-based pricing
             </p>
+            <div className="title">
+              <img src={check} alt="" />
+              Quality work done quickly
+            </div>
+            <p>
+              Find the right freelancer to begin working on your project within
+              minutes.
+            </p>
+            <div className="title">
+              <img src={check} alt="" />
+              Protected payments, every time
+            </div>
+            <p>
+              Always know what you'll pay upfront. Your payment isn't released
+              until you approve the work.
+            </p>
+            <div className="title">
+              <img src={check} alt="" />
+              24/7 support
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+            <div className="title">
+              <img src={check} alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates, just project-based pricing
+            </p>
           </div>
           <div className='item'>
-            <video src="" controls></video>
+            <video src={video} controls></video>
           </div>
         </div>
       </div>
