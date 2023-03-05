c0d58eb072a571da3152d935aea637970da62d3c
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-05 20:46:15 +0530

    added explore component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Slide from '../../components/slider/Slide'
 import TrustedBy from '../../components/trustedby/TrustedBy'
 import "./home.css"
 import { cards } from '../../data'
+import { projects } from '../../data'
 import CategoryCard from '../../components/categoryCard/CategoryCard'
 import check from "../../assets/check.png"
 import video from "../../assets/video.mp4"
@@ -68,6 +69,95 @@ const Home = () => {
         </div>
       </div>
 
+      {/* explore cards */}
+      <div className="explore">
+        <div className="container">
+          <h1>Explore the marketplace</h1>
+          <div className="items">
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/graphics-design.d32a2f8.svg"
+                alt=""
+              />
+              <div className="line"></div>
+              <span>Graphics & Design</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/online-marketing.74e221b.svg"
+                alt=""
+              />
+              <div className="line"></div>
+
+              <span>Digital Marketing</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/writing-translation.32ebe2e.svg"
+                alt=""
+              />
+              <div className="line"></div>
+              <span>Writing & Translation</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/video-animation.f0d9d71.svg"
+                alt=""
+              />
+              <div className="line"></div>
+              <span>Video & Animation</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/music-audio.320af20.svg"
+                alt=""
+              />
+              <div className="line"></div>
+              <span>Music & Audio</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/programming.9362366.svg"
+                alt=""
+              />
+              <div className="line"></div>
+              <span>Programming & Tech</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/business.bbdf319.svg"
+                alt=""
+              />
+              <div className="line"></div>
+              <span>Business</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/lifestyle.745b575.svg"
+                alt=""
+              />
+              <div className="line"></div>
+              <span>Lifestyle</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/data.718910f.svg"
+                alt=""
+              />
+              <div className="line"></div>
+              <span>Data</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/photography.01cf943.svg"
+                alt=""
+              />
+              <div className="line"></div>
+              <span>Photography</span>
+            </div>
+          </div>
+        </div>
+      </div>
 
       {/* // second feature component */}
 
@@ -112,7 +202,7 @@ const Home = () => {
       {/* second slider */}
       <Slide slidesToShow={4} arrowsScroll={3}>
         {
-          cards?.map(card => (
+          projects?.map(card => (
             <CategoryCard key={card?.id} item={ card} />
           ))
         }
