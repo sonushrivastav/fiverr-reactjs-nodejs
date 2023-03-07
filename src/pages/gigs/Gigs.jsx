16ed752de72a210907d93518fbf75b3602a7e925
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-07 21:29:17 +0530

    adding css to gigs component

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -4,7 +4,7 @@ const Gigs = () => {
   return (
     <div className='gigs'>
       <div className="container">
-        <span>FIVERR > GRAPHICS & DESIGN</span>
+        <span className='breadcrumbs'>FIVERR > GRAPHICS & DESIGN</span>
         <h1>AI Artists</h1>
         <p>
           Explore the boundaries of art and technology with Fiverr's AI artists 
