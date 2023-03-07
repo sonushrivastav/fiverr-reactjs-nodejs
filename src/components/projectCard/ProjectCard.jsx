eb53e44117a274838e741faa8592d6b2258c2798
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-07 18:41:58 +0530

    change some boxshadow to card  of pojectcard

--- src/components/projectCard/ProjectCard.jsx
+++ src/components/projectCard/ProjectCard.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import "./projectcard.css"
-const ProjectCard = ({item}) => {
+import React from "react";
+import { Link } from "react-router-dom";
+import "./projectcard.css";
+const ProjectCard = ({ item }) => {
   return (
-    <div className='projectCard'>
-      <img src={item?.img} alt="" />
-          <div className='info'>
-              <img src={item?.pp} alt="" />
-              <div className='texts'>
-                  <h2>{item?.cat}</h2>
-                  <span>{ item?.username}</span>
-              </div>
+    <Link to="/" className="link">
+      <div className="projectCard">
+        <img src={item?.img} alt="" />
+        <div className="info">
+          <img src={item?.pp} alt="" />
+          <div className="texts">
+            <h2>{item?.cat}</h2>
+            <span>{item?.username}</span>
+          </div>
+        </div>
       </div>
-    </div>
-  )
-}
+    </Link>
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
