de7263901863552b90bfa9f4cd5a2f31634bb506
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-07 21:21:04 +0530

    done with the gig component structure

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -2,8 +2,32 @@ import React from 'react'
 import "./gigs.css"
 const Gigs = () => {
   return (
-    <div>
-      Gigs
+    <div className='gigs'>
+      <div className="container">
+        <span>FIVERR > GRAPHICS & DESIGN</span>
+        <h1>AI Artists</h1>
+        <p>
+          Explore the boundaries of art and technology with Fiverr's AI artists 
+        </p>
+        <div className='menu'>
+          <div className='left'>
+            <span>Budged</span>
+            <input type="text" placeholder='min'/>
+            <input type="text" placeholder='max'/>
+            <button>Apply</button>
+          </div>
+          <div className='right'>
+            <span className='sortBy'>SortBy</span>
+            <span className='sortType'>Best Selling</span>
+            <img src="" alt="" />
+            <div className='rightMenu'>
+              <span>Newest</span>
+              <span>Best Selling</span>
+            </div>
+          </div>
+        </div>
+    </div>
+      
     </div>
   )
 }
