ac5a84588591def8b66a1d7a22dcc25c812e1a28
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-09 19:39:31 +0530

    changing name of class gigs page

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -4,20 +4,20 @@ import downimg from "../../assets/down.png"
 const Gigs = () => {
   return (
     <div className='gigs'>
-      <div className="container">
+      <div className="gigscontainer">
         <span className='breadcrumbs'>FIVERR > GRAPHICS & DESIGN</span>
         <h1>AI Artists</h1>
         <p>
           Explore the boundaries of art and technology with Fiverr's AI artists 
         </p>
         <div className='menu'>
-          <div className='left'>
+          <div className='leftmenu'>
             <span>Budged</span>
             <input type="text" placeholder='min'/>
             <input type="text" placeholder='max'/>
             <button>Apply</button>
           </div>
-          <div className='right'>
+          <div className='rightmenu'>
             <span className='sortBy'>SortBy</span>
             <span className='sortType'>Best Selling</span>
             <img src={downimg} alt="" />
