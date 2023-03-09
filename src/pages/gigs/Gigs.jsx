f593e52cfb67cca26b6a177b5762b57a675407f1
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-09 20:00:37 +0530

    added logic for sorting option change dynamic

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,45 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import "./gigs.css";
-import downimg from "../../assets/down.png"
+import downimg from "../../assets/down.png";
 const Gigs = () => {
-
-  const [open, setOpen] = useState(false)
-  
+  const [open, setOpen] = useState(false);
+  const [sort, setSort] = useState("sales");
 
   return (
-    <div className='gigs'>
+    <div className="gigs">
       <div className="gigscontainer">
-        <span className='breadcrumbs'>FIVERR > GRAPHICS & DESIGN</span>
+        <span className="breadcrumbs">FIVERR > GRAPHICS & DESIGN</span>
         <h1>AI Artists</h1>
         <p>
-          Explore the boundaries of art and technology with Fiverr's AI artists 
+          Explore the boundaries of art and technology with Fiverr's AI artists
         </p>
-        <div className='menu'>
-          <div className='leftmenu'>
+        <div className="menu">
+          <div className="leftmenu">
             <span>Budged</span>
-            <input type="text" placeholder='min'/>
-            <input type="text" placeholder='max'/>
+            <input type="text" placeholder="min" />
+            <input type="text" placeholder="max" />
             <button>Apply</button>
           </div>
-          <div className='rightmenucontainer'>
-            <span className='sortBy'>SortBy</span>
-            <span className='sortType'>Best Selling</span>
-            <img src={downimg} alt="" onClick={()=> setOpen(!open)}/>
-            {
-              open && (
-
-            <div className='rightMenu'>
-              <span>Newest</span>
-              <span>Best Selling</span>
-            </div>
-              )
-            }
+          <div className="rightmenucontainer">
+            <span className="sortBy">SortBy</span>
+            <span className="sortType">{sort === "sales" ? "Best Selling" : "Newest"}</span>
+            <img src={downimg} alt="" onClick={() => setOpen(!open)} />
+            {open && (
+              <div className="rightMenu">
+                {sort === "sales" ? (
+                  <span onClick={() => setSort("abc")}>Newest</span>
+                ) : (
+                  <span onClick={() => setSort("sales")}>Best Selling</span>
+                )}
+              </div>
+            )}
           </div>
         </div>
+      </div>
     </div>
-      
-    </div>
-  )
-}
+  );
+};
 
-export default Gigs
+export default Gigs;
