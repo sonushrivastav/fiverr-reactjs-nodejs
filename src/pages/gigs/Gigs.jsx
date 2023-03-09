f88505241166332e84994ca8f39a15a7446cb8ec
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-09 19:51:28 +0530

    write logic for open of sorting button

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./gigs.css";
 import downimg from "../../assets/down.png"
 const Gigs = () => {
+
+  const [open, setOpen] = useState(false)
+  
+
   return (
     <div className='gigs'>
       <div className="gigscontainer">
@@ -20,11 +24,16 @@ const Gigs = () => {
           <div className='rightmenucontainer'>
             <span className='sortBy'>SortBy</span>
             <span className='sortType'>Best Selling</span>
-            <img src={downimg} alt="" />
+            <img src={downimg} alt="" onClick={()=> setOpen(!open)}/>
+            {
+              open && (
+
             <div className='rightMenu'>
               <span>Newest</span>
               <span>Best Selling</span>
             </div>
+              )
+            }
           </div>
         </div>
     </div>
