66376530afd55ba311272011f32aaaa487e150a5
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-10 21:17:30 +0530

    added gig slider and images

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -35,6 +35,30 @@ const Gig = () => {
               alt=""
             />
           </Slider>
+          <h2>About This Gig</h2>
+          <p>I use an AI program to create images based on text prompts. This
+            means I can help you to create a vision you have through a textual
+            description of your scene without requiring any reference images.
+            Some things I've found it often excels at are: Character portraits
+            (E.g. a picture to go with your DnD character) Landscapes (E.g.
+            wallpapers, illustrations to compliment a story) Logos (E.g. Esports
+            team, business, profile picture) You can be as vague or as
+            descriptive as you want. Being more vague will allow the AI to be
+            more creative which can sometimes result in some amazing images. You
+            can also be incredibly precise if you have a clear image of what you
+            want in mind. All of the images I create are original and will be
+            found nowhere else. If you have any questions you're more than
+            welcome to send me a message.</p>
+          
+          <div className='seller'>
+         <h2>About The Seller</h2>
+            <div className='user'>
+            <img src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <div className='info'>
+                <span>Anna Bell</span>
+              </div>
+         </div>
+          </div>
         </div>
         <div className='gigRight'></div>
       </div>
