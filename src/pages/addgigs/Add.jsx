e909da46cd5e65a7fff19a2daad6fa06ff7e6608
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-14 19:56:43 +0530

    added new gig component

--- src/pages/addgigs/Add.jsx
+++ src/pages/addgigs/Add.jsx
@@ -3,7 +3,20 @@ import "./add.css"
 const Add = () => {
   return (
     <div className='add'>
-      add
+      <div className='addContainer'>
+        <h1>Add new Gig</h1>
+        <div className='sections'>
+          <label htmlFor="">Title</label>
+          <input type="text" placeholder="e.g. I will do something I'm really good at" />
+          <label htmlFor="">Category</label>
+          <select name="cats" id="cats">
+            <option value="design">Design</option>
+            <option value="web">Web Development</option>
+            <option value="animation">Animation</option>
+            <option value="music">Music</option>
+          </select>
+        </div>
+      </div>
     </div>
   )
 }
