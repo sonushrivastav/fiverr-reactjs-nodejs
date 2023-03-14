621b22d7c776a8d8dcb381d2268137a49ddf2bd0
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-14 20:12:50 +0530

    adding input lftside

--- src/pages/addgigs/Add.jsx
+++ src/pages/addgigs/Add.jsx
@@ -6,7 +6,7 @@ const Add = () => {
       <div className='addContainer'>
         <h1>Add new Gig</h1>
         <div className='sections'>
-          <div className='info'>
+          <div className='left'>
           <label htmlFor="">Title</label>
           <input type="text" placeholder="e.g. I will do something I'm really good at" />
           <label htmlFor="">Category</label>
@@ -21,25 +21,25 @@ const Add = () => {
           <label htmlFor="">Upload Images</label>
           <input type="file" multiple/>
           <label htmlFor="">Description</label>
-          <textarea name="" id="" placeholder="Brief descriptions to introduce your service to customers" cols="0" rows="16"></textarea>
+          <textarea name="" id="" placeholder="Brief descriptions to introduce your service to customers" cols="30" rows="16"></textarea>
           <button>Create</button>
         </div>
-        <div className='gigDetails'>
+        <div className='right'>
           <label htmlFor="">Service Title</label>
           <input type="text" placeholder="e.g. One-page web design"/>
           <label htmlFor="">Short Description</label>
           <textarea name="" id="" placeholder="Short description of your service" cols="30" rows="10"></textarea>
           <label htmlFor="">Delivery Time (e.g. 3 days)</label>
-          <input type="number" />
+          <input type="number" min={1}/>
           <label htmlFor="">Revision Number</label>
-          <input type="number" />
+          <input type="number" min={1}/>
           <label htmlFor="">Add Features</label>
           <input type="text" placeholder="e.g. page design" />
             <input type="text" placeholder="e.g. file uploading" />
             <input type="text" placeholder="e.g. setting up a domain" />
             <input type="text" placeholder="e.g. hosting" />
             <label htmlFor="">Price</label>
-            <input type="number" />
+            <input type="number" min={1}/>
           </div>
           </div>
       </div>
