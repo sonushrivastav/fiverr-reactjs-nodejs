6401fe11f579e6b3fc87a060d2c6563f1a2b12c1
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-14 21:20:05 +0530

    connected backend with mongodb databse

--- api/server.js
+++ api/server.js
@@ -1,11 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
+import dotenv from "dotenv"
 const PORT = process.env.PORT || 8000
 
+dotenv.config()
 
-
+const connect = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO)
+        console.log("connected to database!");
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
 
 
 app.listen(PORT, () => {
+    connect()
     console.log(`Backend is running on port no ${PORT}`);
 })
