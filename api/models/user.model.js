fec2758936d2ba9314a719f61383a6d93b8779b8
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-15 19:22:12 +0530

    email and password updated in schema

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -6,7 +6,16 @@ const userSchema = new Schema({
         type: String,
         required: true,
         unique:true
-  }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique:true
+    },
+    password: {
+        type: String,
+        required :true
+    }
 
     
 })
