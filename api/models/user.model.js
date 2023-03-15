7837724d7046f04abc34ae82a64c12faf16ed3e8
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-15 19:20:51 +0530

    creatinng username schema

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 const { Schema } = mongoose
 
 const userSchema = new Schema({
-
+    username: {
+        type: String,
+        required: true,
+        unique:true
+  }
 
     
 })
