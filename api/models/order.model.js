824bffee5823d1b3797e5f2e72b84174ea538469
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-17 20:00:08 +0530

    adding order model

---
+++ api/models/order.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose
+
+const OrderSchema = new Schema({
+    gigId: {
+        type: String,
+        required:true
+    },
+    img: {
+        type: String,
+        required:false,
+    },
+    title: {
+        type: String,
+        required: true,
+        
+    },
+    price: {
+        type: Number,
+        required:true
+    },
+    sellerId: {
+        type: String,
+        required:true
+    }
+
+    
+}, {
+    timestamps:true
+})
+
+export default mongoose.model("Order",OrderSchema)
