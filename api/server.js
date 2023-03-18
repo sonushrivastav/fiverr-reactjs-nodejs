ca4ad2e1ff692f33dd72a18306f660bf187779ab
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-18 22:58:35 +0530

    import all routes in main server file

--- api/server.js
+++ api/server.js
@@ -4,6 +4,11 @@ const app = express();
 import dotenv from "dotenv"
 const PORT = process.env.PORT || 8000;
 import userRoute from "./routes/user.route.js"
+import conversationRoute from "./routes/conversation.route.js"
+import gigRoute from "./routes/gig.route.js"
+import orderRoute from "./routes/order.route.js"
+import messageRoute from "./routes/message.route.js"
+import reviewRoute from "./routes/review.route.js"
 
 dotenv.config()
 
@@ -17,7 +22,12 @@ const connect = async() => {
     
 }
 
-app.use("/api/user", userRoute)
+app.use("/api/users", userRoute)
+app.use("/api/gigs", gigRoute)
+app.use("/api/orders", orderRoute)
+app.use("/api/conversations", conversationRoute)
+app.use("/api/messages", messageRoute)
+app.use("/api/reviews", reviewRoute)
 
 
 app.listen(PORT, () => {
