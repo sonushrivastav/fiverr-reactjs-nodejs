12619d87e078e11e3b919142df0de8e7ad0c40f3
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-18 23:03:59 +0530

    created functiokn for authentication

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,9 @@
 import express from "express"
 const router = express.Router()
+import {register,login,logout} from "../controller/auth.controller.js"
 
-
-router.get("/test",)
+router.post("/register",register)
+router.post("/login", login)
+router.post("/logout",logout)
 
 export default router
