02a0483bd9c66f8376a4f4289f5db20c42c3aa73
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-24 20:27:47 +0530

    jkcgdkfd

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,11 @@
 export const register = (req, res) => {
     try {
-        
+        const newUser = new User({
+            username: "test",
+            email: "test",
+            password: "test",
+            country:"test"
+        })
     } catch (error) {
         res.status(500).send("Something went Wrong!")
     }
