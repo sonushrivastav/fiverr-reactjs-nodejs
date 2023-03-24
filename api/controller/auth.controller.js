5d3d20472efd3e6aff32cf03c87e10b565ffba70
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-24 20:38:08 +0530

    sdsklsd

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,11 +1,8 @@
 export const register = async(req, res) => {
     try {
-        const newUser = new User({
-            username: "test",
-            email: "test",
-            password: "test",
-            country:"test"
-        })
+        const newUser = new User(
+            req.body
+        )
 
         await newUser.save();
         res.status(201).send("user has been created!")
