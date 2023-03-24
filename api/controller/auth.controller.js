58ead133d11bfba8cc67fcae00b3c63fdb0855c6
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-24 20:26:47 +0530

    upadting auth folder with authentication

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,5 +1,9 @@
 export const register = (req, res) => {
-    
+    try {
+        
+    } catch (error) {
+        res.status(500).send("Something went Wrong!")
+    }
 }
 export const login = (req, res) => {
     
