238475f663fc2fd12565d6907170be0be42fbfb6
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-24 20:46:17 +0530

    smndsld

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,24 +1,19 @@
-import User from "../models/user.model.js"
-import bcrypt from "bcrypt"
-
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
 
 export const register = async (req, res) => {
-    try {
-        const newUser = new User(
-            req.body
-        )
+  try {
+    const hash = bcrypt.hashSync(req.body.password, 5);
+    const newUser = new User({
+      password: hash,
+      ...req.body,
+    });
 
-        await newUser.save();
-        res.status(201).send("user has been created!")
-    } catch (error) {
-        res.status(500).send("Something went Wrong!")
-    }
-}
-export const login = (req, res) => {
-    
-}
-export const logout = (req, res) => {
-    
-}
-
-
+    await newUser.save();
+    res.status(201).send("user has been created!");
+  } catch (error) {
+    res.status(500).send("Something went Wrong!");
+  }
+};
+export const login = (req, res) => {};
+export const logout = (req, res) => {};
