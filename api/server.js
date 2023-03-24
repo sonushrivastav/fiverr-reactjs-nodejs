0e1032c70fe8b3da679c22054121136758431bd0
Mern-stack-developer <sonushrivastav1811@gmail.com>
2023-03-24 20:35:58 +0530

    adfsuyuvksb

--- api/server.js
+++ api/server.js
@@ -9,6 +9,7 @@ import gigRoute from "./routes/gig.route.js"
 import orderRoute from "./routes/order.route.js"
 import messageRoute from "./routes/message.route.js"
 import reviewRoute from "./routes/review.route.js"
+import authRoute from "./routes/auth.route.js"
 
 dotenv.config()
 
@@ -28,6 +29,7 @@ app.use("/api/orders", orderRoute)
 app.use("/api/conversations", conversationRoute)
 app.use("/api/messages", messageRoute)
 app.use("/api/reviews", reviewRoute)
+app.use("/api/auth", authRoute)
 
 
 app.listen(PORT, () => {
